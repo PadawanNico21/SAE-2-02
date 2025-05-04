749484743f93ba3e30c0af8eb25ee19bd09dc963
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2025-05-04 19:49:46 +0200

    format, commentaires & derniers tests

--- src/automates.py
+++ src/automates.py
@@ -1,4 +1,7 @@
 def defauto_alphabet(defaut="ab") -> set:
+    """
+    Demande de saisir l'alphabet d'un automate
+    """
     print("Veuilliez donnez l'alphabet de votre automate (par ex: abcd)")
     alphabet = input(f"({defaut}) > ")
 
@@ -14,6 +17,9 @@ def defauto_alphabet(defaut="ab") -> set:
 
 
 def defauto_etats() -> set:
+    """
+    Demande de saisir les différents états d'un automate
+    """
     etats = set()
 
     print("Veuilliez saisir les états de votre automate, 'fin' pour stopper la saisie")
@@ -38,6 +44,9 @@ def defauto_etats() -> set:
 
 
 def defauto_etats_selection(etats: set, type: str) -> set:
+    """
+    Demande de séléctionner des états d'un automate pour une raison {type}
+    """
     print(
         f"Veuilliez saisir les états {type} de votre automate, 'fin' pour stopper la saisie"
     )
@@ -66,7 +75,10 @@ def defauto_etats_selection(etats: set, type: str) -> set:
 
 
 def defauto_transition_pour(etat: int, etats: set, alphabet: set) -> list:
-    print(f"Saisissez les transitions pour l'etat {etat}")
+    """
+    Demande de saisir les transitions pour l'état {etat}
+    """
+    print(f"Saisissez les transitions pour l'état {etat}")
 
     transitions = []
 
@@ -99,6 +111,9 @@ def defauto_transition_pour(etat: int, etats: set, alphabet: set) -> list:
 
 
 def defauto_transitions(etats: set, alphabet: set) -> list:
+    """
+    Demande de saisir les transitions d'un automate (pour tous les états)
+    """
     transitions = []
     while True:
         print(
@@ -125,6 +140,9 @@ def defauto_transitions(etats: set, alphabet: set) -> list:
 
 
 def defauto() -> dict:
+    """
+    Demande de saisir un automate et le renvoie
+    """
     print("----- ALPHABET ----")
     alphabet = defauto_alphabet()
 
@@ -153,7 +171,6 @@ def lirelettre(
 ) -> set[int]:
     """
     Renvoie les états dans lesquels on peut arriver en partant d'un état de `etats` et en lisant la lettre `lettre`
-    Renvoie les états dans lesquels on peut arriver en partant d'un état de `etats` et en lisant la lettre `lettre`
     """
     resultat: set[int] = set()
 
@@ -165,6 +182,9 @@ def lirelettre(
 
 
 def liremot(transitions: list[list[int | str]], etats: set[int], mot: str) -> set[int]:
+    """
+    Lis un mot à partir des transitions
+    """
     etats_suivants = set(etats)
 
     for lettre in mot:
@@ -176,10 +196,16 @@ def liremot(transitions: list[list[int | str]], etats: set[int], mot: str) -> se
 
 
 def accepte(auto: dict, mot: str) -> bool:
+    """
+    Renvoie True si l'automate accepte le mot
+    """
     return len(liremot(auto["transitions"], auto["I"], mot) & auto["F"]) > 0
 
 
 def deterministe(auto: dict) -> bool:
+    """
+    Renvoie vrai si l'automate passé en paramètre est deterministe
+    """
     transitions_vus = dict()
 
     for etat in auto["etats"]:
@@ -201,7 +227,9 @@ def deterministe(auto: dict) -> bool:
 
 
 def determinise(auto: dict) -> dict:
-
+    """
+    Determinise l'automate passé en paramètre
+    """
     depart = frozenset(auto["I"])
 
     a_faire = [depart]
@@ -247,6 +275,9 @@ def determinise(auto: dict) -> dict:
 
 
 def renommage(auto: dict) -> dict:
+    """
+    Renomme les états de l'automate passé en paramètre
+    """
     auto_renom = {
         "alphabet": auto["alphabet"],
         "etats": [],
@@ -283,6 +314,9 @@ def renommage(auto: dict) -> dict:
 
 
 def inter(auto1: dict, auto2: dict) -> dict:
+    """
+    Réalise l'intersection des automates auto1 et auto2
+    """
     if not deterministe(auto1):
         auto1 = renommage(determinise(auto1))
 
@@ -348,6 +382,9 @@ def ajoute_etats(auto: dict) -> dict:
 
 
 def difference(auto1: dict, auto2: dict) -> dict:
+    """
+    Réalise la différence des automates auto1 et auto2
+    """
     if not complet(auto1):
         auto1 = complete(auto1)
     if not complet(auto2):
@@ -377,6 +414,9 @@ def difference(auto1: dict, auto2: dict) -> dict:
 
 
 def langage_accept(automate: dict, n: int) -> set:
+    """
+    Renvoie tous les mots de longueur <= n accepté par l'automate passé en paramètre
+    """
     initial = {}
 
     for etat in automate["I"]:
@@ -390,6 +430,12 @@ def langage_accept(automate: dict, n: int) -> set:
 
 
 def langage_accept_rec(automate: dict, positions: dict, resultat_mots: set, n: int):
+    """
+    ATTENTION: Merci d'utiliser la fonction langage_accept
+
+    Recursive
+    Renvoie tous les mots de longueur <= n accepté par l'automate passé en paramètre
+    """
     if n <= 0:
         return resultat_mots
 
@@ -413,6 +459,9 @@ def langage_accept_rec(automate: dict, positions: dict, resultat_mots: set, n: i
 
 
 def liste_transitions_manquantes(auto: dict) -> dict:
+    """
+    Liste toutes les transitions manquantes de l'automate pour qu'il soit considiré complet
+    """
     manquants = {}
 
     for etat in auto["etats"]:
@@ -430,6 +479,9 @@ def liste_transitions_manquantes(auto: dict) -> dict:
 
 
 def complet(auto: dict) -> bool:
+    """
+    Renvoie vrais si l'automate passé en paramètre est complet
+    """
     for lettres in liste_transitions_manquantes(auto).values():
         if len(lettres) != 0:
             return False
@@ -438,6 +490,9 @@ def complet(auto: dict) -> bool:
 
 
 def complete(auto: dict) -> dict:
+    """
+    Complete l'automate pour qu'il soit complet
+    """
     manquants = liste_transitions_manquantes(auto)
 
     auto_modif = dict(auto)
@@ -453,6 +508,9 @@ def complete(auto: dict) -> dict:
 
 
 def complement(auto: dict) -> dict:
+    """
+    Réalise l'inverse de l'automate
+    """
     auto_complet = complete(auto)
 
     for etat in auto_complet["etats"]:
@@ -464,6 +522,9 @@ def complement(auto: dict) -> dict:
 
 
 def prefixe(auto: dict) -> dict:
+    """
+    Renvoie l'automate qui accepte tous les préfixes de celui passé en paramètre
+    """
     auto_prefixe = dict(auto)
 
     auto_prefixe["F"] = set(auto_prefixe["etats"])
@@ -472,6 +533,9 @@ def prefixe(auto: dict) -> dict:
 
 
 def suffixe(auto: dict) -> dict:
+    """
+    Renvoie l'automate qui accepte tous les suffixes de celui passé en paramètre
+    """
     auto_suffixe = dict(auto)
 
     auto_suffixe["I"] = set(auto_suffixe["etats"])
@@ -480,10 +544,16 @@ def suffixe(auto: dict) -> dict:
 
 
 def facteur(auto: dict) -> dict:
+    """
+    Renvoie l'automate qui accepte tous les facteurs de celui passé en paramètre
+    """
     return suffixe(prefixe(auto))
 
 
 def mirroir(auto: dict) -> dict:
+    """
+    Renvoie l'automate qui accepte tous les mots mirroirs de celui passé en paramètre
+    """
     auto_mirroir = dict(auto)
 
     auto_mirroir["I"], auto_mirroir["F"] = auto_mirroir["F"], auto_mirroir["I"]
@@ -495,12 +565,18 @@ def mirroir(auto: dict) -> dict:
 
 
 def position_classe(classes: dict, etat: int):
+    """
+    Renvoie la position de l'etat dans les classes d'équivalences
+    """
     for key, etats in classes.items():
         if etat in etats:
             return key
 
 
 def equivalence(auto: dict) -> list:
+    """
+    Calcules les classe d'équivalences de l'automate passé en paramètre
+    """
     classes = {"AUTRES": auto["etats"] - auto["F"], "FINAUX": auto["F"]}
     taille = 0
 
@@ -532,12 +608,18 @@ def equivalence(auto: dict) -> list:
 
 
 def vers_classe(etat: int, classes: list) -> tuple:
+    """
+    Convertis l'état dans sa classe d'équivalence
+    """
     for classe in classes:
         if etat in classe:
             return classe
 
 
 def minimise(auto: dict):
+    """
+    Minimise l'automate passé en paramètre
+    """
     classes_equivalences = equivalence(auto)
 
     initiaux = set()
@@ -582,15 +664,15 @@ if __name__ == "__main__":
     }
 
     auto1 = {
-        "alphabet": ["a", "b"],
-        "etats": [0, 1],
+        "alphabet": {"a", "b"},
+        "etats": {0, 1},
         "transitions": [[0, "a", 0], [0, "b", 1], [1, "b", 1], [1, "a", 1]],
-        "I": [0],
-        "F": [1],
+        "I": {0},
+        "F": {1},
     }
 
     auto3 = {
-        "alphabet": ["a", "b"],
+        "alphabet": {"a", "b"},
         "etats": {
             0,
             1,
@@ -620,24 +702,24 @@ if __name__ == "__main__":
     print("Mirroir auto:", mirroir(auto))
 
     auto0 = {
-        "alphabet": ["a", "b"],
-        "etats": [0, 1, 2, 3],
+        "alphabet": {"a", "b"},
+        "etats": {0, 1, 2, 3},
         "transitions": [[0, "a", 1], [1, "a", 1], [1, "b", 2], [2, "a", 3]],
-        "I": [0],
-        "F": [3],
+        "I": {0},
+        "F": {3},
     }
 
     auto1 = {
-        "alphabet": ["a", "b"],
-        "etats": [0, 1],
+        "alphabet": {"a", "b"},
+        "etats": {0, 1},
         "transitions": [[0, "a", 0], [0, "b", 1], [1, "b", 1], [1, "a", 1]],
-        "I": [0],
-        "F": [1],
+        "I": {0},
+        "F": {1},
     }
 
     auto2 = {
-        "alphabet": ["a", "b"],
-        "etats": [0, 1],
+        "alphabet": {"a", "b"},
+        "etats": {0, 1},
         "transitions": [[0, "a", 0], [0, "a", 1], [1, "b", 1], [1, "a", 1]],
         "I": {0, 1},
         "F": {1},
@@ -646,23 +728,7 @@ if __name__ == "__main__":
     print(deterministe(auto0))
     print(deterministe(auto2))
 
-    auto2_det = {
-        "alphabet": {"a", "b"},
-        "I": [[0]],
-        "transitions": [
-            [[0], "a", [0, 1]],
-            [[0, 1], "a", [0, 1]],
-            [[0, 1], "b", [1]],
-            [[1], "a", [1]],
-            [[1], "b", [1]],
-        ],
-        "etats": [[0], [0, 1], [1]],
-        "F": [[0, 1], [1]],
-    }
-
-    print(
-        renommage(auto2_det)
-    )
+    print(renommage(determinise(auto2)))
 
     auto4 = {
         "alphabet": {"a", "b"},
@@ -691,8 +757,33 @@ if __name__ == "__main__":
         "F": {0, 1},
     }
 
+    auto6 = {
+        "alphabet": {"a", "b"},
+        "etats": {0, 1, 2, 3, 4, 5},
+        "transitions": [
+            [0, "a", 4],
+            [0, "b", 3],
+            [1, "a", 5],
+            [1, "b", 5],
+            [2, "a", 5],
+            [2, "b", 2],
+            [3, "a", 1],
+            [3, "b", 0],
+            [4, "a", 1],
+            [4, "b", 2],
+            [5, "a", 2],
+            [5, "b", 5],
+        ],
+        "I": {0},
+        "F": {0, 1, 2, 5},
+    }
+
     print("\nIntersection :")
     print((inter(auto4, auto5)))
 
     print("\nDifférence")
     print(difference(auto4, auto5))
+
+    print("Minimisation:")
+    print(minimise(auto6))
+    print(renommage(minimise(auto6)))
