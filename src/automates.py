31c788a050e46f28fc9c99c8442c5002f6fe223b
PadawanNico21 <63717504+PadawanNico21@users.noreply.github.com>
2025-05-04 15:15:40 +0200

    Format + fix nom

--- src/automates.py
+++ src/automates.py
@@ -2,9 +2,12 @@ def defauto():
     # A faire
     pass
 
-def lirelettre(transitions: list[list[int | str]], etats: set[int], lettre: str) -> set[int]:
+
+def lirelettre(
+    transitions: list[list[int | str]], etats: set[int], lettre: str
+) -> set[int]:
     """
-        Renvoie les états dans lesquels on peut arriver en partant d'un état de `etats` et en lisant la lettre `lettre`
+    Renvoie les états dans lesquels on peut arriver en partant d'un état de `etats` et en lisant la lettre `lettre`
     """
     resultat: set[int] = set()
 
@@ -13,6 +16,7 @@ def lirelettre(transitions: list[list[int | str]], etats: set[int], lettre: str)
             resultat.add(transition[2])
 
     return resultat
+
 
 def liremot(transitions: list[list[int | str]], etats: set[int], mot: str) -> set[int]:
     etats_suivants = set(etats)
@@ -26,44 +30,45 @@ def liremot(transitions: list[list[int | str]], etats: set[int], mot: str) -> se
 
 
 def accepte(auto: dict, mot: str) -> bool:
-    return len(liremot(auto['transitions'], auto['I'], mot) & auto['F']) > 0
+    return len(liremot(auto["transitions"], auto["I"], mot) & auto["F"]) > 0
 
 
 def deterministe(auto: dict) -> bool:
     transitions_vus = dict()
 
-    for etat in auto['etats']:
+    for etat in auto["etats"]:
         transitions_vus[etat] = dict()
 
-        for lettre in auto['alphabet']:
+        for lettre in auto["alphabet"]:
             transitions_vus[etat][lettre] = 0
 
-    for transition in auto['transitions']:
+    for transition in auto["transitions"]:
         etat, lettre = transition[0], transition[1]
 
         if transitions_vus[etat][lettre] == 0:
-            transitions_vus[etat][lettre] = 1 
+            transitions_vus[etat][lettre] = 1
 
-        else : return False
+        else:
+            return False
 
     return True
 
 
 def determinise(auto: dict) -> dict:
-    
-    depart = frozenset(auto['I'])
+
+    depart = frozenset(auto["I"])
 
     a_faire = [depart]
     faits = set([depart])
 
-    transitions = list(auto['transitions'])
+    transitions = list(auto["transitions"])
 
     nouvelles_transtions = []
 
     while len(a_faire) > 0:
         etat = a_faire.pop()
 
-        for lettre in auto['alphabet']:
+        for lettre in auto["alphabet"]:
             nouvel_etat = set()
             for t in transitions:
                 if t[0] in etat and t[1] == lettre:
@@ -79,18 +84,18 @@ def determinise(auto: dict) -> dict:
             if val not in faits:
                 a_faire.append(val)
                 faits.add(val)
-    
+
     etats_finaux = set()
 
     for transition in nouvelles_transtions:
-        if len(auto['F'] & transition[2]) > 0:
+        if len(auto["F"] & transition[2]) > 0:
             etats_finaux.add(transition[2])
 
     return {
-        "alphabet": set(auto['alphabet']),
+        "alphabet": set(auto["alphabet"]),
         "etats": faits,
-        "transtitions": nouvelles_transtions,
-        "I": depart,
+        "transitions": nouvelles_transtions,
+        "I": [depart],
         "F": etats_finaux,
     }
 
@@ -108,28 +113,28 @@ if __name__ == "__main__":
     print("accepte aba:", accepte(auto, "aba"))
     print("accepte ba:", accepte(auto, "ba"))
 
-    auto0 ={
-        "alphabet":['a','b'],
-        "etats": [0,1,2,3],
-        "transitions":[[0,'a',1],[1,'a',1],[1,'b',2],[2,'a',3]], 
-        "I":[0],
-        "F":[3]
+    auto0 = {
+        "alphabet": ["a", "b"],
+        "etats": [0, 1, 2, 3],
+        "transitions": [[0, "a", 1], [1, "a", 1], [1, "b", 2], [2, "a", 3]],
+        "I": [0],
+        "F": [3],
     }
 
-    auto1 ={
-        "alphabet":['a','b'],
-        "etats": [0,1],
-        "transitions":[[0,'a',0],[0,'b',1],[1,'b',1],[1,'a',1]], 
-        "I":[0],
-        "F":[1]
+    auto1 = {
+        "alphabet": ["a", "b"],
+        "etats": [0, 1],
+        "transitions": [[0, "a", 0], [0, "b", 1], [1, "b", 1], [1, "a", 1]],
+        "I": [0],
+        "F": [1],
     }
 
-    auto2={
-        "alphabet":['a','b'],
-        "etats": [0,1],
-        "transitions":[[0,'a',0],[0,'a',1],[1,'b',1],[1,'a',1]], 
-        "I": {0,1},
-        "F":{1}
+    auto2 = {
+        "alphabet": ["a", "b"],
+        "etats": [0, 1],
+        "transitions": [[0, "a", 0], [0, "a", 1], [1, "b", 1], [1, "a", 1]],
+        "I": {0, 1},
+        "F": {1},
     }
 
     print(deterministe(auto0))
