de422dd3dfeee930af6c5374eb454c8c05860098
EthanPln <ethanpoluno@gmail.com>
2025-05-04 17:18:51 +0200

    Fix renommage + intersection bug

--- src/automates.py
+++ src/automates.py
@@ -83,7 +83,8 @@ def renommage(auto: dict) -> dict:
     }
     liste_etats = list(auto["etats"])
 
-    auto_renom['I'].add(liste_etats.index(auto['I'][0]))
+    for etat_initial in auto['I']:
+         auto_renom['I'].add(liste_etats.index(etat_initial))
 
     for etat_final in auto['F']:
         auto_renom['F'].add(liste_etats.index(etat_final))
@@ -100,6 +101,79 @@ def renommage(auto: dict) -> dict:
     auto_renom["etats"] = set(auto_renom["etats"])
 
     return auto_renom
+
+
+def inter(auto1: dict, auto2: dict) -> dict:
+    if not deterministe(auto1):
+        auto1 = renommage(determinise(auto1))
+
+    if not deterministe(auto2):
+        auto2 = renommage(determinise(auto2))
+    
+    auto_inter = {
+        "alphabet": auto1['alphabet'],
+        "etats": set(),
+        "transitions": [],
+        "I": set(),
+        "F": set()
+    }
+    
+    transitions = []
+    debut_transi_a1 = debut_transition(auto1)
+    debut_transi_a2 = debut_transition(auto2)
+
+    for i in range(len(auto1['transitions'])):
+        if debut_transi_a1[i] in debut_transi_a2:
+            j = debut_transi_a2.index(debut_transi_a1[i])
+            transition1 = auto1['transitions'][i]
+            transition2 = auto2['transitions'][j]
+            lettre = transition1[1]
+
+            transitions.append([(transition1[0], transition2[0]), lettre, (transition1[2], transition2[2])])
+    
+    auto_inter['transitions'] = transitions
+    ajoute_etats(auto_inter)
+
+    # Un seul etat car déterministe
+    for etat_i in auto1['I']:
+        etat_initial1 = etat_i
+
+    for etat_i in auto2['I']:
+        etat_initial2 = etat_i
+
+    auto_inter['I'].add((etat_initial1, etat_initial2))
+
+    for etat_final in auto1['F']:
+        for transition in auto_inter['transitions']:
+
+            if transition[0][0] == etat_final and transition[0][1] in auto2['F']:
+                auto_inter['F'].add((etat_final, transition[0][1]))
+
+            elif transition[2][0] == etat_final and transition[2][1] in auto2['F']:
+                auto_inter['F'].add((etat_final, transition[2][1]))
+    
+    return auto_inter
+
+
+def debut_transition(auto: dict) -> list:
+    """
+    Prend en paramètre un automate et renvoie une liste des débuts de transition: [p, a]
+    """
+    debut_transi = []
+
+    for transition in auto['transitions']:
+        debut_transi.append(transition[:2])
+    
+    return debut_transi
+
+
+def ajoute_etats(auto: dict) -> dict:
+    """
+    Prend en paramètre un automate avec des transitions et rajoute ses etats manquants 
+    """
+    for transition in auto['transitions']:
+        auto['etats'].add(transition[0])
+        auto['etats'].add(transition[2]) 
 
 
 if __name__ == "__main__":
@@ -149,3 +223,22 @@ if __name__ == "__main__":
     }
 
     print(renommage(auto2_det)) # à modifier par print(renommage(determinise(auto2_det)))
+
+    auto4 = {
+        "alphabet":{'a','b'},
+        "etats": {0,1,2,},
+        "transitions":[[0,'a',1],[1,'b',2],[2,'b',2],[2,'a',2]], 
+        "I":{0},
+        "F":{2}
+    }
+    
+    auto5 = {
+        "alphabet":{'a','b'},
+        "etats": {0,1,2},
+        "transitions":[[0,'a',0],[0,'b',1],[1,'a',1],[1,'b',2],[2,'a',2],[2,'b',0]],
+        "I":{0},
+        "F":{0,1}
+    }
+
+    print("\nIntersection :")
+    print(inter(auto4,auto5))
